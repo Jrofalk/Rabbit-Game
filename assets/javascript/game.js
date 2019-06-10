63050d5eb8d4d55f7f07e5f2e5c996b42612787f
John Falk <jrfalk@outlook.com>
2019-06-10 12:31:27 -0400

    Added click function to buttons and win and loss scenarios

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,61 @@
-//Everything happens inside of here, waits until document loads
 $(document).ready(function() {
 
   //Generates random target number to be displayed
   var randomTarget = Math.floor(Math.random() * 102) + 19;
   $('#target-number').html('<h1>Random Number:</h1> ' + randomTarget);
+  
+    
+    var score = 0;
+    var wins = 0;
+    var losses = 0;
+  
+  $('#score').text(score);
 
-  //Generates random number for each crystal. You need to change these to add given number to what is displayed in score, not just replace it.
   var crystalOne = Math.floor(Math.random() * 12) + 1;
-    $('#button-one').on('click', function() {
-      $('#score').html(crystalOne);
-    });
-
   var crystalTwo = Math.floor(Math.random() * 12) + 1;
-    $('#button-two').on('click', function() {
-      $('#score').html(crystalTwo);
-    });
-        
   var crystalThree = Math.floor(Math.random() * 12) + 1;
-    $('#button-three').on('click', function() {
-      $('#score').html(crystalThree);
-    });
+  var crystalFour = Math.floor(Math.random() * 12) + 1;
 
-    var crystalFour = Math.floor(Math.random() * 12) + 1;
-    $('#button-four').on('click', function() {
-      $('#score').html(crystalFour);
-    });
-        
+  function win(){
+      wins++;
+      $('#total-wins').text(wins);
+  }
 
+  function loss(){
+      losses++;
+      $('#total-losses').text(losses);
+  }
 
   
 
+  $('#button-one').on ('click', function(){
+    score = score + crystalOne;
+    $('#score').text(score); 
+      if(score === randomTarget){
+          win();
+      }
+      else if(score > randomTarget){
+          loss();
+      }
+  })
 
+  $('#button-two').on ('click', function(){
+    score = score + crystalTwo;
+    $('#score').text(score);
+  })
 
+  $('#button-three').on ('click', function(){
+    score = score + crystalThree;
+    $('#score').text(score);
+  })
 
-
-
-
-
-
-
-
-
+  $('#button-four').on ('click', function(){
+    score = score + crystalFour;
+    $('#score').text(score);
+  })
 
 
 
 
 });
+
