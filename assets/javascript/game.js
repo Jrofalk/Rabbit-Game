e987a882ab5a629a1f9dc486d39095c2fa0c3f7d
John Falk <jrfalk@outlook.com>
2019-06-10 17:04:28 -0400

    Added images to take place of buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,57 +1,84 @@
 $(document).ready(function() {
 
-  //Generates random target number to be displayed
+//Generates random target number to be displayed
   var randomTarget = Math.floor(Math.random() * 102) + 19;
   $('#target-number').html('<h1>Random Number:</h1> ' + randomTarget);
-  
-    
-    var score = 0;
-    var wins = 0;
-    var losses = 0;
-  
-  $('#score').text(score);
 
   var crystalOne = Math.floor(Math.random() * 12) + 1;
   var crystalTwo = Math.floor(Math.random() * 12) + 1;
   var crystalThree = Math.floor(Math.random() * 12) + 1;
   var crystalFour = Math.floor(Math.random() * 12) + 1;
 
+  var score = 0;
+  var wins = 0;
+  var losses = 0;
+
+  function restart(){
+    randomTarget = Math.floor(Math.random() * 102) + 19;
+    $('#target-number').html('<h1>Random Number:</h1> ' + randomTarget);
+    crystalOne = Math.floor(Math.random() * 12) + 1;
+    crystalTwo = Math.floor(Math.random() * 12) + 1;
+    crystalThree = Math.floor(Math.random() * 12) + 1;
+    crystalFour = Math.floor(Math.random() * 12) + 1;
+    score = 0;
+    $('#score').text(score);
+  }
+
   function win(){
-      wins++;
-      $('#total-wins').text(wins);
+    wins++;
+    $('#total-wins').text(wins);
+    restart();
   }
 
   function loss(){
-      losses++;
-      $('#total-losses').text(losses);
+    losses++;
+    $('#total-losses').text(losses);
+    restart();
   }
 
-  
 
   $('#button-one').on ('click', function(){
     score = score + crystalOne;
     $('#score').text(score); 
       if(score === randomTarget){
-          win();
+        win();
       }
       else if(score > randomTarget){
-          loss();
+        loss();
       }
   })
 
   $('#button-two').on ('click', function(){
     score = score + crystalTwo;
     $('#score').text(score);
+      if(score === randomTarget){
+        win();
+      }
+      else if(score > randomTarget){
+        loss();
+      }
   })
 
   $('#button-three').on ('click', function(){
     score = score + crystalThree;
     $('#score').text(score);
+      if(score === randomTarget){
+        win();
+      }
+      else if(score > randomTarget){
+        loss();
+      }
   })
 
   $('#button-four').on ('click', function(){
     score = score + crystalFour;
     $('#score').text(score);
+      if(score === randomTarget){
+        win();
+      }
+      else if(score > randomTarget){
+        loss();
+      }
   })
 
 
