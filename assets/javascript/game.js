5238bb128d943cfa224b67482bfa4ce77cb28162
John Falk <jrfalk@outlook.com>
2019-06-11 13:28:54 -0400

    Finalized appearance of rabbits, added instructions and associated functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,44 @@
 $(document).ready(function() {
 
-//Generates random target number to be displayed
+  //Generates random target number to be displayed
   var randomTarget = Math.floor(Math.random() * 102) + 19;
-  $('#target-number').html('<h1>Random Number:</h1> ' + randomTarget);
-
-  var crystalOne = Math.floor(Math.random() * 12) + 1;
-  var crystalTwo = Math.floor(Math.random() * 12) + 1;
-  var crystalThree = Math.floor(Math.random() * 12) + 1;
-  var crystalFour = Math.floor(Math.random() * 12) + 1;
-
+  $('#target-number').html('<h1>Target:</h1> ' + randomTarget);
+  //Generates random value for each rabbit
+  var rabbitOne = Math.floor(Math.random() * 12) + 1;
+  var rabbitTwo = Math.floor(Math.random() * 12) + 1;
+  var rabbitThree = Math.floor(Math.random() * 12) + 1;
+  var rabbitFour = Math.floor(Math.random() * 12) + 1;
+  //Variables for score, wins, and losses
   var score = 0;
   var wins = 0;
   var losses = 0;
-
+  //Function to reset game
   function restart(){
     randomTarget = Math.floor(Math.random() * 102) + 19;
-    $('#target-number').html('<h1>Random Number:</h1> ' + randomTarget);
-    crystalOne = Math.floor(Math.random() * 12) + 1;
-    crystalTwo = Math.floor(Math.random() * 12) + 1;
-    crystalThree = Math.floor(Math.random() * 12) + 1;
-    crystalFour = Math.floor(Math.random() * 12) + 1;
+    $('#target-number').html('<h1>Target:</h1> ' + randomTarget);
+    rabbitOne = Math.floor(Math.random() * 12) + 1;
+    rabbitTwo = Math.floor(Math.random() * 12) + 1;
+    rabbitThree = Math.floor(Math.random() * 12) + 1;
+    rabbitFour = Math.floor(Math.random() * 12) + 1;
     score = 0;
     $('#score').text(score);
   }
-
+  //Funtion to record wins
   function win(){
     wins++;
     $('#total-wins').text(wins);
     restart();
   }
-
+  //Function to record losses
   function loss(){
     losses++;
     $('#total-losses').text(losses);
     restart();
   }
 
-
+  //Allows rabbit images to be clicked and updates values
   $('#button-one').on ('click', function(){
-    score = score + crystalOne;
+    score = score + rabbitOne;
     $('#score').text(score); 
       if(score === randomTarget){
         win();
@@ -49,7 +49,7 @@ $(document).ready(function() {
   })
 
   $('#button-two').on ('click', function(){
-    score = score + crystalTwo;
+    score = score + rabbitTwo;
     $('#score').text(score);
       if(score === randomTarget){
         win();
@@ -60,7 +60,7 @@ $(document).ready(function() {
   })
 
   $('#button-three').on ('click', function(){
-    score = score + crystalThree;
+    score = score + rabbitThree;
     $('#score').text(score);
       if(score === randomTarget){
         win();
@@ -71,7 +71,7 @@ $(document).ready(function() {
   })
 
   $('#button-four').on ('click', function(){
-    score = score + crystalFour;
+    score = score + rabbitFour;
     $('#score').text(score);
       if(score === randomTarget){
         win();
@@ -80,6 +80,10 @@ $(document).ready(function() {
         loss();
       }
   })
+  //Displays and hides instructions
+  $('#dropdownMenu1').on('click', function(){
+      $('.hidden').toggle();
+  });
 
 
 
